28e8c25e0a3b48fb4d98ddee0d6d44c7760b60b1
Marcin Mańkowski <marcin.mankowski@dka.io>
2023-03-07 16:13:11 +0100

    add removing favorites from list

--- components/FavoritesList/FavoritesList.tsx
+++ components/FavoritesList/FavoritesList.tsx
@@ -18,17 +18,34 @@ const FavoritesList = () => {
       setFavoritePhotos(photos);
     };
     fetchFavoritePhotos();
-  }, []);
+  }, [favoritePhotos]);
+
+  const handleRemoveFromFavorites = (photoId: number) => {
+    const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+
+    localStorage.setItem(
+      "favorites",
+      JSON.stringify(favorites.filter((favId: number) => favId !== photoId))
+    );
+  };
+
+  const handleClearFavorites = () => {
+    localStorage.setItem("favorites", JSON.stringify([]));
+  };
 
   return (
     <div>
       <h2>Favorite Photos COMPONENT</h2>
       <button onClick={() => router.back()}>Go back</button>
+      <button onClick={handleClearFavorites}>Clear favorite list</button>
       <div>
         {favoritePhotos.map((photo, i) => (
           <div key={photo.id}>
             <h3>{photo.title != "" ? photo.title : "No title on that :("}</h3>
             <Image src={photo.url} alt={photo.title} width={500} height={500} />
+            <button onClick={() => handleRemoveFromFavorites(photo.id)}>
+              Remove from favorites
+            </button>
           </div>
         ))}
       </div>
